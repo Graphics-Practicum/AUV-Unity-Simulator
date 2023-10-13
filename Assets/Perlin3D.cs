32dd0105625ccdb9855c212eeacde3db43a0173b
LEVR02 <scao663@gmail.com>
2023-10-13 14:29:33 -0400

    finish 3D Perlin noise implemnetation (realized 5D was just a product of my mind on a light fever)

--- Assets/Perlin3D.cs
+++ Assets/Perlin3D.cs
@@ -4,65 +4,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class Vector5 : object
-{
-    public double[] values;
-    public Vector5(double v1, double v2, double v3, double v4, double v5)
-    {
-        this.values = new double[] { v1, v2, v3, v4, v5 };
-    }
-    public double dot(Vector5 other)
-    {
-        return this.values[0] * other.values[0] + this.values[1] * other.values[1] + this.values[2] * other.values[2] + this.values[3] * other.values[3] + this.values[4] * other.values[4];
-    }
-    public double getNorm()
-    {
-        return Math.Sqrt(this.values.Sum());
-    }
-    public void normalize()
-    {
-        this.values[0] = this.values[0] / this.getNorm();
-        this.values[1] = this.values[1] / this.getNorm();
-        this.values[2] = this.values[2] / this.getNorm();
-        this.values[3] = this.values[3] / this.getNorm();
-        this.values[4] = this.values[4] / this.getNorm();
-    }
-}
-
 public class Perlin3D : MonoBehaviour
 {
     public int worldXBound = 1000;
     public int worldYBound = 1000;
-    public int worldZBound = 1000;
-    public int cameraXBound = 1920;
-    public int cameraYBound = 1080; // These are arbitrary and should be set with actual values given 
-    public int worldResolution = 16;
-    public int resolution = 256; // These determine the grid sizes for the perlin noise generation, might want to split them up into four or five separate pieces though
-    public Vector5[,,,,] perlin;
+    public int worldZBound = 1000; // These are arbitrary and should be set with actual values given 
+    public int worldResolution = 16; // These determine the grid sizes for the perlin noise generation, might want to split them up 
+    public Vector3[,,] perlin;
 
     // Start is called before the first frame update
-    Vector5 randomUnitVec()
+    Vector3 randomUnitVec()
     {
-        Vector5 vec = new Vector5(UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f));
-        vec.normalize();
-        return vec;
+        Vector3 vec = new Vector3(UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f), UnityEngine.Random.Range(0.0f, 1.0f));
+        return vec.normalized;
     }
     void Start()
     {
-        perlin = new Vector5[worldResolution, worldResolution, worldResolution, resolution, 256];
+        perlin = new Vector3[worldResolution, worldResolution, worldResolution];
         for (var x = 0; x < worldResolution; x++)
         {
             for (var y = 0; y < worldResolution; y++)
             {
                 for (var z = 0; z < worldResolution; z++)
                 {
-                    for (var x1 = 0; x1 < resolution; x1++)
-                    {
-                        for (var y1 = 0; y1 < resolution; y1++)
-                        {
-                            perlin[x, y, z, x1, y1] = randomUnitVec(); //forgive me
-                        }
-                    }
+                    perlin[x, y, z] = randomUnitVec();
                 }
             }
         }
@@ -79,30 +44,34 @@ public class Perlin3D : MonoBehaviour
         }
         return start + (3 * along * along - 2 * along * along * along) * (end - start); // Using smoothstep here https://en.wikipedia.org/wiki/Smoothstep
     }
-    double getValueAtCoords(double x, double y, double z, double x1, double y1)
+    double getGrad(double x, double y, double z, int x_grid, int y_grid, int z_grid)
+    {
+        Vector3 grid = perlin[x_grid, y_grid, z_grid];
+        return (x - x_grid) * grid.x + (y - y_grid) * grid.y + (z - z_grid) * grid.z;
+    }
+    double getValueAtCoords(double x, double y, double z)
     {
         double scaledX = x / (worldXBound + 1) * worldResolution;
         double scaledY = y / (worldYBound + 1) * worldResolution;
         double scaledZ = z / (worldZBound + 1) * worldResolution;
-        double scaledX1 = x1 / (cameraXBound + 1) * worldResolution;
-        double scaledY1 = y1 / (cameraYBound + 1) * worldResolution;
         int x_less = (int)Math.Floor(scaledX);
         int x_greater = x_less + 1;
         int y_less = (int)Math.Floor(scaledY);
         int y_greater = y_less + 1;
         int z_less = (int)Math.Floor(scaledZ);
         int z_greater = z_less + 1;
-        int x1_less = (int)Math.Floor(scaledX1);
-        int x1_greater = x1_less + 1;
-        int y1_less = (int)Math.Floor(scaledY1);
-        int y1_greater = y1_less + 1;
         double dx = scaledX - x_less;
         double dy = scaledY - y_less;
         double dz = scaledZ - z_less;
-        double dx1 = scaledX1 - x1_less;
-        double dx2 = scaledY1 - y1_less;
-
-
+        double lll = getGrad(x, y, z, x_less, y_less, z_less);
+        double llg = getGrad(x, y, z, x_less, y_less, z_greater);
+        double lgl = getGrad(x, y, z, x_less, y_greater, z_less);
+        double lgg = getGrad(x, y, z, x_less, y_greater, z_greater);
+        double gll = getGrad(x, y, z, x_greater, y_less, z_less);
+        double glg = getGrad(x, y, z, x_greater, y_less, z_greater);
+        double ggl = getGrad(x, y, z, x_greater, y_greater, z_less);
+        double ggg = getGrad(x, y, z, x_greater, y_greater, z_greater);
+        return interp(interp(interp(lll, gll, x), interp(lgl, ggl, x), y), interp(interp(llg, glg, x), interp(lgg, ggg, x), y), z); // man i hope this is right
     }
 
     // Update is called once per frame
