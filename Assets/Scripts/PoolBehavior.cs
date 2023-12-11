84cbd678cb9363bad92733e8d7af3f47c57c8c29
Sarah Ha <nh285@cornell.edu>
2023-12-11 17:03:00 -0500

    create generator script

--- Assets/Scripts/PoolBehavior.cs
+++ Assets/Scripts/PoolBehavior.cs
@@ -7,21 +7,12 @@ public class PoolBehavior : MonoBehaviour
     public GameObject[] walls;
     public GameObject[] noWalls;
 
-    public bool[] test;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        UpdatePool(test);
-    }
-
-    // Update is called once per frame
-    void UpdatePool(bool[] status)
+    public void UpdatePool(bool[] status)
     {
         for(int i = 0; i<status.Length; i++)
         {
             noWalls[i].SetActive(status[i]);
-            walls[i].SetActive(status[i]);
+            walls[i].SetActive(!status[i]);
         }
         
     }
---
+++ Assets/Scripts/PoolGenerator.cs
@@ -0,0 +1,145 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolGenerator : MonoBehaviour
+{
+    public class Cell
+    {
+        public bool visited = false;
+        public bool[] status = new bool[4];
+    }
+
+    public Vector2 size;
+    public int startPos = 0;
+    public GameObject pool;
+    public Vector2 offset;
+    public Vector2 totalSize;
+
+    List<Cell> board;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MazeGenerator();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GeneratePool()
+    {
+        for(int i= 0; i<totalSize.x; i++)
+        {
+            for(int j= 0; j <totalSize.y; j++)
+            {
+                var newPool = Instantiate(pool, new Vector3(i*offset.x, 0, -j*offset.y), Quaternion.identity, transform).GetComponent<PoolBehavior>();
+                newPool.UpdatePool(board[Mathf.FloorToInt(i+j*size.x)].status);
+            }
+        }
+    }
+
+    void MazeGenerator()
+    {
+        board = new List<Cell>();
+
+        for(int i = 0; i<size.x; i++)
+        {
+            for(int j = 0;j < size.y; j++)
+            {
+                board.Add(new Cell());
+            }
+        }
+        int currentCell = startPos;
+
+        Stack<int> path= new Stack<int>();
+
+        int k = 0;
+
+        while(k<10)
+        {
+            k++;
+
+            board[currentCell].visited = true;
+            List<int> neighbors = CheckNeighbors(currentCell);
+
+            if(neighbors.Count == 0)
+            {
+                if(path.Count == 0)
+                {
+                    break;
+                }
+                else
+                {
+                    currentCell = path.Pop();
+                }
+            }
+            else
+            {
+                path.Push(currentCell);
+
+                int newCell = neighbors[Random.Range(0,neighbors.Count)];
+
+                if(newCell > currentCell)
+                {
+                    if(newCell - 1 == currentCell)
+                    {
+                        board[currentCell].status[1] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[3] = true;
+                    }
+                    else
+                    {
+                        board[currentCell].status[2] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[0] = true;
+                    }
+                }
+                else
+                {
+                    if(newCell - 1 == currentCell)
+                    {
+                        board[currentCell].status[3] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[1] = true;
+                    }
+                    else
+                    {
+                        board[currentCell].status[0] = true;
+                        currentCell = newCell;
+                        board[currentCell].status[2] = true;
+                    }
+                }
+            }
+
+        }
+        GeneratePool();
+    }
+
+    List<int> CheckNeighbors(int cell)
+    {
+        List<int> neighbors = new List<int>();
+        if (cell - size.x >= 0 && !board[Mathf.FloorToInt(cell - size.x)].visited)
+        {
+            neighbors.Add(Mathf.FloorToInt(cell - size.x));
+        }
+
+        if (cell + size.x < board.Count && !board[Mathf.FloorToInt(cell + size.x)].visited)
+        {
+            neighbors.Add(Mathf.FloorToInt(cell + size.x));
+        }
+
+        if ((cell+1) % size.x !=0 && !board[Mathf.FloorToInt(cell + 1)].visited)
+        {
+            neighbors.Add(Mathf.FloorToInt(cell + 1));
+        }
+
+        if (cell % size.x !=0 && !board[Mathf.FloorToInt(cell - 1)].visited)
+        {
+            neighbors.Add(Mathf.FloorToInt(cell -1));
+        }
+        return neighbors;
+    }
+}
